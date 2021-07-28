ea3ee901faf2a1b325368f59bf7b5046534890d7
megute <alinkapirjinka@gmail.com>
2021-07-28 22:40:29 +0300

    omg

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    $('.reviews__slider').slick();
-});
-
 $(document).ready(function() {
     $('.header__burger').click(function(event) {
         $('.header__burger,.head__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+});
+
+$(document).ready(function(){
+    $('.reviews__slider').slick();
 });
